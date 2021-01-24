b477ca5f413ee8796f3853d775cfef028fbc3461
jackzhoustudy <zypstudy@163.com>
2021-01-24 12:28:41 +0800

    '服务提供者'

---
+++ eureka-client/src/main/java/top/inson/springcloud/web/HelloController.java
@@ -0,0 +1,24 @@
+package top.inson.springcloud.web;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@Slf4j
+@RestController
+@RequestMapping(value = "/hello")
+public class HelloController {
+
+
+    @GetMapping("/sayHello")
+    public String sayHello(@RequestParam(required = false, defaultValue = "jingjitree") String username){
+        log.info("进入了欢迎页面" + username);
+        return "hello world!" + username;
+    }
+
+
+}
